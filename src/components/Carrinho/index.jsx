8abb277bb44639c7e4391cc3e16b9b3907180339
Julia Gama de Oliveira <86054348+JuliaDevgama@users.noreply.github.com>
2021-10-01 14:48:31 -0300

    fix:rounded

--- src/components/Carrinho/index.jsx
+++ src/components/Carrinho/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
 export default function Carrinho({currentSale}) {
-    const purchase = currentSale.reduce((a,b)=>a+(b[0].price*1),0)
+    const purchase = currentSale.reduce((a,b)=>a+(b[0].price*1),0)//
     const disc = currentSale.reduce((a,b)=>a+(b[0].discount*1),0)
     return (
         <div>
-            <p>Valor da compra: R${purchase}</p>
-            <p>Cliente economizou: R${disc}</p>
+            <p>Valor da compra: R${Math.round(purchase* 100) / 100}</p>
+            <p>Cliente economizou: R${Math.round(disc* 100) / 100}</p>
         </div>
     )
 }
