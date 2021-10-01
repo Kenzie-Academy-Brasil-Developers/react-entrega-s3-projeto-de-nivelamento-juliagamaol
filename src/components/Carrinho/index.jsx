e05c2671da1c74aceb81cd00b38db4e2b801d302
Julia Gama de Oliveira <86054348+JuliaDevgama@users.noreply.github.com>
2021-10-01 12:37:37 -0300

    feat:with name in currentSale

--- src/components/Carrinho/index.jsx
+++ src/components/Carrinho/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
-export default function Carrinho({currentSale}) {
+export default function Carrinho({currentSale:{name}}) {
     const purchase = currentSale.reduce((a,b)=>a+b[0].price,0).toFixed(2)
     const disc = currentSale.reduce((a,b)=>a+b[0].discount,0).toFixed(2)
     return (
         <div>
+            <h3>{name}</h3>
             <p>Valor da compra: R${purchase}</p>
             <p>Cliente economizou: R${disc}</p>
         </div>
