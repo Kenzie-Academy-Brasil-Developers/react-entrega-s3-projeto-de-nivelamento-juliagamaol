d0ab555566f688a140e3e5bbd68694ac39c3d7d2
Julia Gama de Oliveira <86054348+JuliaDevgama@users.noreply.github.com>
2021-10-01 12:50:26 -0300

    fix:fixed currentSale name

--- src/components/Carrinho/index.jsx
+++ src/components/Carrinho/index.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 
-export default function Carrinho({currentSale:{name}}) {
+export default function Carrinho({currentSale}) {
     const purchase = currentSale.reduce((a,b)=>a+b[0].price,0).toFixed(2)
     const disc = currentSale.reduce((a,b)=>a+b[0].discount,0).toFixed(2)
     return (
-        <div>
-            <h3>{name}</h3>
-            <p>Valor da compra: R${purchase}</p>
-            <p>Cliente economizou: R${disc}</p>
-        </div>
+        <>
+            <strong>Carrrinho</strong>
+            {currentSale.map(item=>(
+                <div>
+                    <span>{item[0].name}</span>
+                    <p>Valor da compra: R${purchase}</p>
+                    <p>Cliente economizou: R${disc}</p>
+                </div>
+            ))}            
+        </>
     )
 }
